c852a7f3832dc009ad5b92dc160281df2c420287
Pietro <dart.murtas@yahoo.it>
2022-07-19 16:50:47 +0200

    slots and intent classifier layer done in classifiers.py

--- models/classifiers.py
+++ models/classifiers.py
@@ -1,1 +1,31 @@
 import torch.nn as nn
+
+
+class IntentClassifier(nn.Module):
+    """
+    Intent classifier layer
+    """
+
+    def __init__(self, input_dim, n_intent_labels, dropout_rate=0.1):
+        super(IntentClassifier, self).__init__()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.linear = nn.Linear(input_dim, n_intent_labels)
+
+    def forward(self, x):
+        x = self.dropout(x)
+        return self.linear(x)
+
+
+class SlotsClassifier(nn.Module):
+    """
+    Slots classifier layer
+    """
+
+    def __init__(self, input_dim, n_slots_labels, dropout_rate=0.1):
+        super(IntentClassifier, self).__init__()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.linear = nn.Linear(input_dim, n_slots_labels)
+
+    def forward(self, x):
+        x = self.dropout(x)
+        return self.linear(x)
