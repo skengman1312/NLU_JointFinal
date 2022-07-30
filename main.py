8caf9c2a8d7b3217ced1611619daa612aa74b4fe
Pietro <dart.murtas@yahoo.it>
2022-07-30 12:21:53 +0200

    fixes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import training
+from dataloader import load_dataset
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +8,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    train = load_dataset()
+    tr = training.JointTrainer("", train_dataset= train)
+    tr.train()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
