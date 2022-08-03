0544917065d30814ebd36828dcdb19699242f957
skengman1312 <dart.murtas@yahoo.it>
2022-08-03 18:04:51 +0200

    added dev test split for ATIS

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    train_data = load_dataset()
-    dev_data = load_dataset(mode="valid")
-    test_data = load_dataset(mode = "test")
+    train_data = load_dataset(dataset="ATIS")
+    dev_data = load_dataset(mode="valid", dataset= "ATIS")
+    test_data = load_dataset(mode = "test", dataset="ATIS")
     t = training.JointTrainer(args="", train_dataset=train_data, dev_dataset=dev_data, test_dataset=test_data)
     t.train()
     t.eval("test")
